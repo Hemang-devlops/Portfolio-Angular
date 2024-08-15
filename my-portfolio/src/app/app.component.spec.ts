9e6c1c8f2efcd513f37b0336f50936e6f93c0de8
Hemang-devlops <136294289+Hemang-devlops@users.noreply.github.com>
2024-08-15 11:26:17 +0000

    making standalone project

--- my-portfolio/src/app/app.component.spec.ts
+++ my-portfolio/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'myPortfolio' title`, () => {
+  it(`should have as title 'my-portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myPortfolio');
+    expect(app.title).toEqual('my-portfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, myPortfolio');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-portfolio');
   });
 });
